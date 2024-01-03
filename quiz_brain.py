f07e527e6849ad468115b850ea67d6332bf138db
Vijay Soundaram <vijay6206@gmail.com>
2024-01-02 22:32:14 -0700

    adding quiz-game

---
+++ quiz_brain.py
@@ -0,0 +1,20 @@
+class QuizBrain():
+
+    def __init__(self,q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            return False
+
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+        self.question_number += 1
+
+
+
